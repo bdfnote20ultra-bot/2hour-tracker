b8fa71ab9eb189cf69c0eed01a1e84d250c236f6
brianfuit7-beep <brianfuit7@gmail.com>
2026-05-12 03:50:41 -0500

    Add files via upload

---
+++ src/musicLibraryData.js
@@ -0,0 +1,34 @@
+export const MUSIC_LIBRARY = [
+  {
+    id: "nirvana-smells-like-teen-spirit-audio",
+    title: "Smells Like Teen Spirit",
+    artist: "Nirvana",
+    genre: "Rock",
+    type: "audio",
+    src: "/music-library/rock/Nirvana - Smells Like Teen Spirit (Official Music Video).mp3"
+  },
+  {
+    id: "nirvana-smells-like-teen-spirit-video",
+    title: "Smells Like Teen Spirit (Official Music Video)",
+    artist: "Nirvana",
+    genre: "Rock",
+    type: "video",
+    src: "/music-library/rock/Nirvana - Smells Like Teen Spirit (Official Music Video).mp4"
+  },
+  {
+    id: "50-cent-many-men-audio",
+    title: "Many Men (Wish Death)",
+    artist: "50 Cent",
+    genre: "Rap",
+    type: "audio",
+    src: "/music-library/rap/50 Cent - Many Men (Wish Death) (Dirty Version).mp3"
+  },
+  {
+    id: "50-cent-many-men-video",
+    title: "Many Men (Wish Death) (Dirty Version)",
+    artist: "50 Cent",
+    genre: "Rap",
+    type: "video",
+    src: "/music-library/rap/50 Cent - Many Men (Wish Death) (Dirty Version).mp4"
+  }
+];

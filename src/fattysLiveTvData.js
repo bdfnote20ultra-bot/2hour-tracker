74bfcb811b6868f3604ac5b0fa4f8119dc505dea
brianfuit7-beep <brianfuit7@gmail.com>
2026-05-14 01:04:05 -0500

    yo
    yo

--- src/fattysLiveTvData.js
+++ src/fattysLiveTvData.js
@@ -13,7 +13,7 @@
 
 export const FUITS_LIVE_TV_PLAYLIST = {
   title: "FUITS LIVE TV WORLD",
-  publicChannelUrl: "https://soviet-gps-piece-several.trycloudflare.com",
+  publicChannelUrl: "https://tomato-deputy-ground-elementary.trycloudflare.com",
   localPlaylistPath: "T:\\FattysLiveTV\\Playlists\\FuitsLiveTV\\fuits-live-tv-world.m3u",
   items: FATTYS_LIVE_TV
 };

29cf52ec576ac3522b0e15caaed4de8a3aca2f63
brianfuit7-beep <brianfuit7@gmail.com>
2026-05-14 00:31:19 -0500

    HI
    HI

--- src/fattysLiveTvData.js
+++ src/fattysLiveTvData.js
@@ -13,7 +13,7 @@
 
 export const FUITS_LIVE_TV_PLAYLIST = {
   title: "FUITS LIVE TV WORLD",
-  publicChannelUrl: "https://forestry-extract-gloves-hidden.trycloudflare.com",
+  publicChannelUrl: "https://soviet-gps-piece-several.trycloudflare.com",
   localPlaylistPath: "T:\\FattysLiveTV\\Playlists\\FuitsLiveTV\\fuits-live-tv-world.m3u",
   items: FATTYS_LIVE_TV
 };

d99cdace4cd6cd3e8ac2f5d9e6ab6a5fe1dbc46a
brianfuit7-beep <brianfuit7@gmail.com>
2026-05-15 04:16:23 -0500

    hi
    hi

--- src/fattysLiveTvData.js
+++ src/fattysLiveTvData.js
@@ -13,7 +13,7 @@
 
 export const FUITS_LIVE_TV_PLAYLIST = {
   title: "FUITS LIVE TV WORLD",
-  publicChannelUrl: "https://road-thumbnail-drop-wanted.trycloudflare.com",
+  publicChannelUrl: "https://progress-entering-magazines-riders.trycloudflare.com",
   localPlaylistPath: "T:\\FattysLiveTV\\Playlists\\FuitsLiveTV\\fuits-live-tv-world.m3u",
   items: FATTYS_LIVE_TV
 };

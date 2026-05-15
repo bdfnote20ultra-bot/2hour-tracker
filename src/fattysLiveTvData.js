c7de0275d6c7bcb711f07fe5fd42acdfe35399b8
brianfuit7-beep <brianfuit7@gmail.com>
2026-05-15 01:28:58 -0500

    hi
    hi

--- src/fattysLiveTvData.js
+++ src/fattysLiveTvData.js
@@ -13,7 +13,7 @@
 
 export const FUITS_LIVE_TV_PLAYLIST = {
   title: "FUITS LIVE TV WORLD",
-  publicChannelUrl: "https://blonde-sep-actual-homepage.trycloudflare.com",
+  publicChannelUrl: "https://assume-cdt-formats-oak.trycloudflare.com",
   localPlaylistPath: "T:\\FattysLiveTV\\Playlists\\FuitsLiveTV\\fuits-live-tv-world.m3u",
   items: FATTYS_LIVE_TV
 };

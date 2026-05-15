3a80114aa44ffd876f5956b6b2d2c1832be85840
brianfuit7-beep <brianfuit7@gmail.com>
2026-05-15 02:19:09 -0500

    hi
    hi

--- src/fattysLiveTvData.js
+++ src/fattysLiveTvData.js
@@ -13,7 +13,7 @@
 
 export const FUITS_LIVE_TV_PLAYLIST = {
   title: "FUITS LIVE TV WORLD",
-  publicChannelUrl: "https://eat-dept-song-win.trycloudflare.com",
+  publicChannelUrl: "https://along-rising-sandy-salmon.trycloudflare.com",
   localPlaylistPath: "T:\\FattysLiveTV\\Playlists\\FuitsLiveTV\\fuits-live-tv-world.m3u",
   items: FATTYS_LIVE_TV
 };

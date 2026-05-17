89fd0925c337df6034caeb1785284321639f76e2
brianfuit7-beep <brianfuit7@gmail.com>
2026-05-17 15:13:19 -0500

    hi
    hi

--- src/fattysLiveTvData.js
+++ src/fattysLiveTvData.js
@@ -13,7 +13,7 @@
 
 export const FUITS_LIVE_TV_PLAYLIST = {
   title: "FUITS LIVE TV WORLD",
-  publicChannelUrl: "https://creating-density-nightlife-yrs.trycloudflare.com",
+  publicChannelUrl: "https://safely-shirts-designation-cooked.trycloudflare.com",
   localPlaylistPath: "T:\\FattysLiveTV\\Playlists\\FuitsLiveTV\\fuits-live-tv-world.m3u",
   items: FATTYS_LIVE_TV
 };

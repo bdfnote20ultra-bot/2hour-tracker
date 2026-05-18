c062fd39dc5a1dac65df689b347a02b9ff267797
brianfuit7-beep <brianfuit7@gmail.com>
2026-05-17 19:30:43 -0500

    HI
    HI

--- src/fattysLiveTvData.js
+++ src/fattysLiveTvData.js
@@ -13,7 +13,7 @@
 
 export const FUITS_LIVE_TV_PLAYLIST = {
   title: "FUITS LIVE TV WORLD",
-  publicChannelUrl: "https://safely-shirts-designation-cooked.trycloudflare.com",
+  publicChannelUrl: "https://singer-sandra-kelkoo-aluminium.trycloudflare.com",
   localPlaylistPath: "T:\\FattysLiveTV\\Playlists\\FuitsLiveTV\\fuits-live-tv-world.m3u",
   items: FATTYS_LIVE_TV
 };

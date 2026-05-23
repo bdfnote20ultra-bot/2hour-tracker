a6679d0500000ba7333d5194d28451d647b3d067
brianfuit7-beep <brianfuit7@gmail.com>
2026-05-22 19:09:55 -0500

    hi
    hi

--- src/fattysLiveTvData.js
+++ src/fattysLiveTvData.js
@@ -13,7 +13,7 @@
 
 export const FUITS_LIVE_TV_PLAYLIST = {
   title: "FUITS LIVE TV WORLD",
-  publicChannelUrl: "https://allowing-allowing-harvey-removing.trycloudflare.com",
+  publicChannelUrl: "https://reference-intl-upgrades-restore.trycloudflare.com",
   localPlaylistPath: "T:\\FattysLiveTV\\Playlists\\FuitsLiveTV\\fuits-live-tv-world.m3u",
   items: FATTYS_LIVE_TV
 };

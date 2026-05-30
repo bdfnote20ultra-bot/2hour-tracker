62c7a9c74384dee7a3bc1a09c03a99c7aa9744bb
brianfuit7-beep <brianfuit7@gmail.com>
2026-05-30 15:12:57 -0500

    OK
    OK

--- src/fattysLiveTvData.js
+++ src/fattysLiveTvData.js
@@ -13,7 +13,7 @@
 
 export const FUITS_LIVE_TV_PLAYLIST = {
   title: "FUITS LIVE TV WORLD",
-  publicChannelUrl: "https://ver-videos-eden-treatments.trycloudflare.com",
+  publicChannelUrl: "https://fallen-spokesman-ended-sunset.trycloudflare.com",
   localPlaylistPath: "T:\\FattysLiveTV\\Playlists\\FuitsLiveTV\\fuits-live-tv-world.m3u",
   items: FATTYS_LIVE_TV
 };

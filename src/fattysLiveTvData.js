8cf44b69083da1a3604dbdc4e3339888ac556485
brianfuit7-beep <brianfuit7@gmail.com>
2026-05-31 02:00:57 -0500

    12
    12

--- src/fattysLiveTvData.js
+++ src/fattysLiveTvData.js
@@ -13,7 +13,7 @@
 
 export const FUITS_LIVE_TV_PLAYLIST = {
   title: "FUITS LIVE TV WORLD",
-  publicChannelUrl: "https://camp-jessica-aerial-institutional.trycloudflare.com",
+  publicChannelUrl: "https://cartoon-columnists-perfectly-pediatric.trycloudflare.com",
   localPlaylistPath: "T:\\FattysLiveTV\\Playlists\\FuitsLiveTV\\fuits-live-tv-world.m3u",
   items: FATTYS_LIVE_TV
 };

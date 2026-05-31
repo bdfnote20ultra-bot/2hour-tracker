b1f05724aa5bcb1fd202b38f01bce34dd9e39852
brianfuit7-beep <brianfuit7@gmail.com>
2026-05-31 15:55:27 -0500

    11
    11

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef, useMemo, useCallback } from "react";
+﻿import { useState, useEffect, useRef, useMemo, useCallback } from "react";
 import { MUSIC_LIBRARY } from "./musicLibraryData";
 import { FATTYS_LIVE_TV, FUITS_LIVE_TV_PLAYLIST, APP_COPY_LABEL, APP_COPY_NOTE } from "./fattysLiveTvData";
 
@@ -2070,7 +2070,7 @@ function MusicLibrarySidebar({ accentColor }) {
     { id: "athf", label: "ADULT SWIM ZONE", heading: "ADULT SWIM ZONE", url: "https://www.adultswim.com/streams/aqua-teen-hunger-force", embed: true },
     { id: "youtube", label: "YOUTUBE", heading: "YOUTUBE", url: "https://www.youtube.com/", embed: false },
     { id: "southpark", label: "SOUTH PARK WORLD", heading: "SOUTH PARK WORLD", url: "https://southpark.cc.com/seasons/south-park", embed: false },
-    { id: "jellyfin", label: "FUIT JELLYFIN", heading: "FUIT JELLYFIN", url: "https://fifteen-forbes-shaped-follow.trycloudflare.com/web/", embed: true }
+    { id: "jellyfin", label: "FUIT JELLYFIN", heading: "FUIT JELLYFIN", url: "https://fifty-caps-intelligent-irc.trycloudflare.com/web/", embed: true }
   ];
   const fuitsLiveTvChannels = [
     { id: "channel-a", label: "Channel A" },

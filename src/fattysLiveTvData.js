39f1c4ba89e83023929233f0b5d162328d1b3b01
brianfuit7-beep <brianfuit7@gmail.com>
2026-06-01 07:56:55 -0500

    33
    33

--- src/fattysLiveTvData.js
+++ src/fattysLiveTvData.js
@@ -13,7 +13,7 @@
 
 export const FUITS_LIVE_TV_PLAYLIST = {
   title: "FUITS LIVE TV WORLD",
-  publicChannelUrl: "https://specify-dsc-seek-deployment.trycloudflare.com",
+  publicChannelUrl: "https://dice-kingdom-peoples-tooth.trycloudflare.com",
   localPlaylistPath: "T:\\FattysLiveTV\\Playlists\\FuitsLiveTV\\fuits-live-tv-world.m3u",
   items: FATTYS_LIVE_TV
 };

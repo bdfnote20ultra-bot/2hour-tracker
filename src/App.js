8c57246cd5c9ef2c2ba34ae900e199115651841c
brianfuit7-beep <brianfuit7@gmail.com>
2026-07-01 04:03:44 -0500

    Raise mobile landscape coin ticker

--- src/App.js
+++ src/App.js
@@ -9967,7 +9967,7 @@ if (view === "gambling") {
             --flive-menu-top-offset: -12px;
             --flive-menu-left-offset: 20px;
             --flive-wealth-extra-top-offset: 10px;
-            --flive-ticker-top-scale: 2.12;
+            --flive-ticker-top-scale: 1.32;
             --flive-center-width-scale: 1.06;
             --flive-center-content-scale: .54;
             --flive-tv-width-scale: 1.18;
